68302056a8b49f2508ee5d693a89d569a0ea8e7e
Troy Dai <troy.dai@outlook.com>
2016-10-10 20:58:59 -0700

    wip 1

--- scripts/build-test-image.sh
+++ scripts/build-test-image.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+    echo "Usage: $0 [path]"
+    exit 1
+fi
+
+echo "Build and test image."
+echo "Path: $1"
+
+pushd $1
+
 # build a test images locally use the Docker file at current directory
 # once the images is built, run an interactive container from it and enter the bash
 
@@ -8,3 +18,7 @@ name=test_image_$(date +%Y%m%d-%H%M%S)
 
 docker build -t $name .
 docker run -it $name /bin/bash
+docker rm -f $name
+
+popd
+
